963da618b6b8e409a70bb31f655a63654003d709
Lukas <lukas.blanchot@hotmail.fr>
2023-05-31 13:09:02 +0200

    ✨ ajout plateau

--- snakegame.py
+++ snakegame.py
@@ -1,6 +1,31 @@
 from random import *
+import msvcrt as m
+
+plateau = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+
+case_bonus = [12,18,27,46]
+
+case_malus = [3,8,14,22,43]
+
+joueur1 = 0
+joueur2 = 0
 
 
+def wait():
+    m.getch()
 
-num_de = randint(1, 6)
+
+while plateau[49] == 0:
+
+    print("Joueur 1 : Appuyer sur entrée pour lancer le dé")
+    wait()
+    num_de = randint(1, 6)
+    print(num_de)
+
+    print("Joueur 2 : Appuyer sur entrée pour lancer le dé")
+    wait()
+    num_de = randint(1, 6)
+    print(num_de)
+
+
 
