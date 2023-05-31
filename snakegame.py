3d6329e11c5e74fd7024e0976fe1eae47d1d71bb
Lukas <lukas.blanchot@hotmail.fr>
2023-05-31 14:31:57 +0200

    � Affichage tableau + Mouvement

--- snakegame.py
+++ snakegame.py
@@ -17,16 +17,46 @@ def wait():
 
 def affichePlateau():
     compt = 0
-    ligne = ""
+    ligne = []
+    ligne2 = []
     for i in range(len(plateau) - 1, -1, -1):
-        ligne += str(i)+" "
-        if compt % 10 == 0 :
-            print("\n")
-            print(ligne)
-            ligne =""
+        
+        if compt % 10 == 0 and compt !=0:
+            
+            if compt == 20 or compt == 40:
+                ligne.reverse()
+                print(ligne)
+            else:
+                print(ligne)
+            ligne =[]
+            ligne2 = []
         #print (i)
+        ligne.append(plateau[i])
+        ligne2.append(i)
+        if joueur1 in ligne2  :
+            if joueur1 == joueur2:
+                ligne[joueur1] = 3
+            else :
+                ligne[joueur1] = 1
+                
+
+        if joueur2 in ligne2  :
+            if joueur1 == joueur2:
+                ligne[joueur1] = 3
+            else :
+                
+                ligne[joueur2] = 2
+
         
         compt += 1
+
+        if compt == 50:
+            
+            print(ligne)
+            ligne =[]
+            ligne2 = []
+
+    
 
 while plateau[49] == 0:
 
@@ -38,9 +68,14 @@ while plateau[49] == 0:
     print(num_de)
     joueur1 += num_de
 
+    affichePlateau()
+
 
     print("Joueur 2 : Appuyer sur entrée pour lancer le dé")
     wait()
     num_de = randint(1, 6)
     print(num_de)
     joueur2 += num_de
+
+    affichePlateau()
+    
